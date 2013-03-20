7e9abdb34bbaf580e5a07a5fff25448900bccab9
Andreas Lefkopoulos <andreas.l@adb.se>
2013-03-20 19:37:43 +0100

    Added visual tests for Pet.cs in Program.cs

--- Spo12hLabb1/Program.cs
+++ Spo12hLabb1/Program.cs
@@ -12,26 +12,50 @@ namespace Spo12hLabb1
 
             #region Animal Tester
 
-            Animal myAnimal = new Animal();
-            Console.WriteLine("\nCreated new Animal using the def Constructor\n");
-            Console.WriteLine(myAnimal.ToString());
+            //Animal myAnimal = new Animal();
+            //Console.WriteLine("\nCreated new Animal using the def Constructor\n");
+            //Console.WriteLine(myAnimal.ToString());
             
 
-            Animal myAnimal2 = new Animal("Rocky", 3);
-            Console.WriteLine("\nCreated new Animal using the second Constructor\n");
-            Console.WriteLine(myAnimal2.ToString());
+            //Animal myAnimal2 = new Animal("Rocky", 3);
+            //Console.WriteLine("\nCreated new Animal using the second Constructor\n");
+            //Console.WriteLine(myAnimal2.ToString());
 
-            Animal myAnimal3 = new Animal();
-            Console.WriteLine("\nDu håller på att skapa ett nytt djur");
-            Console.Write("Ange djurets namn: ");
-            myAnimal3.Name = Console.ReadLine();
-            Console.Write("Ange djurets ålder: ");
-            myAnimal3.Age = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nDu har angett:\n");
-            Console.WriteLine(myAnimal3.ToString());
+            //Animal myAnimal3 = new Animal();
+            //Console.WriteLine("\nDu håller på att skapa ett nytt djur");
+            //Console.Write("Ange djurets namn: ");
+            //myAnimal3.Name = Console.ReadLine();
+            //Console.Write("Ange djurets ålder: ");
+            //myAnimal3.Age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("\nDu har angett:\n");
+            //Console.WriteLine(myAnimal3.ToString());
 
             #endregion
 
+            #region Pet Tester
+
+            Pet myPet = new Pet();
+            Console.WriteLine("\nCreated new Pet using the def Constructor\n");
+            Console.WriteLine(myPet.ToString());
+
+
+            Pet myPet2 = new Pet("Rocky", 3, "Andreas", false);
+            Console.WriteLine("\nCreated new Pet using the second Constructor\n");
+            Console.WriteLine(myPet2.ToString());
+
+            Pet myPet3 = new Pet();
+            Console.WriteLine("\nDu håller på att skapa ett nytt Husdjur");
+            Console.Write("Ange djurets namn: ");
+            myPet3.Name = Console.ReadLine();
+            Console.Write("Ange djurets ålder: ");
+            myPet3.Age = int.Parse(Console.ReadLine());
+            Console.Write("Ange Ägarens namn: ");
+            myPet3.Owner = Console.ReadLine();
+            // Häar ska det implementera HUMÖR ------------------------------!!!!!!!!!
+            Console.WriteLine("\nDu har angett:\n");
+            Console.WriteLine(myPet3.ToString());
+
+            #endregion
             Console.ReadLine();
         }
     }
