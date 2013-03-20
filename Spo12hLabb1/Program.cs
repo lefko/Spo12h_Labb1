9faf295d6f5d3c3092e8d4eecd651622c6c5f3be
Andreas Lefkopoulos <andreas.l@adb.se>
2013-03-20 18:45:58 +0100

    Added visual tests in Program.cs for Animal

--- Spo12hLabb1/Program.cs
+++ Spo12hLabb1/Program.cs
@@ -9,6 +9,30 @@ namespace Spo12hLabb1
     {
         static void Main(string[] args)
         {
+
+            #region Animal Tester
+
+            Animal myAnimal = new Animal();
+            Console.WriteLine("\nCreated new Animal using the def Constructor\n");
+            Console.WriteLine(myAnimal.ToString());
+            
+
+            Animal myAnimal2 = new Animal("Rocky", 3);
+            Console.WriteLine("\nCreated new Animal using the second Constructor\n");
+            Console.WriteLine(myAnimal2.ToString());
+
+            Animal myAnimal3 = new Animal();
+            Console.WriteLine("\nDu håller på att skapa ett nytt djur");
+            Console.Write("Ange djurets namn: ");
+            myAnimal3.Name = Console.ReadLine();
+            Console.Write("Ange djurets ålder: ");
+            myAnimal3.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDu har angett:\n");
+            Console.WriteLine(myAnimal3.ToString());
+
+            #endregion
+
+            Console.ReadLine();
         }
     }
 }
