e9c71c3b7b4d5d92513cccf6ff5da3e5d7141207
Andreas Lefkopoulos <andreas.l@adb.se>
2013-03-20 23:36:15 +0100

    Class Dog.cs created and developed

--- Spo12hLabb1/Dog.cs
+++ Spo12hLabb1/Dog.cs
@@ -19,17 +19,45 @@ namespace Spo12hLabb1
 
         // Konstruktorer
 
+        public Dog() : base()
+        {
+            race = "ingen ras angiven";
+        }
+
+         public Dog(string name, int age, string owner, bool mood, string race) : base(name, age, owner, mood)
+        {
+            this.Race = race;
+        }
+
         #endregion
 
         #region --------- Properties ---------
 
         // Properties
 
+        public String Race
+        {
+            get { return race; }
+            set { race = value; }
+        }
+
         #endregion
 
         #region --------- Methods -----------
 
         // Metoder
+
+        public override string ToString()
+        {
+            string dogMood;
+            if (base.Mood == true)
+                dogMood = "Viftar på svansen";
+            else
+                dogMood = "arg och otrevlig";
+
+            string s = "\nDjurets Detaljer: \nNamn: " + Name + "\nÅlder: " + Age + "\nÄgare: " + Owner + "\nRas: " + Race + "\nHumör: " + dogMood;
+            return s;
+        }
 
         #endregion
     }
