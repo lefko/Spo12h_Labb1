6c421bf902d3e8f34ac27502e5c3ac14f1b30541
Andreas Lefkopoulos <andreas.l@adb.se>
2013-03-20 17:25:56 +0100

    Implementation of Class Animal.cs

--- Spo12hLabb1/Animal.cs
+++ Spo12hLabb1/Animal.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,17 +19,51 @@ namespace Spo12hLabb1
 
         // Konstruktorer
 
+        public Animal():this("Not named", 0)
+        {
+        }
+
+        public Animal(string nm, int a)
+        {
+            Name = nm;
+            Age = a;
+        }
+
         #endregion
 
         #region --------- Properties ---------
 
         // Properties
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value;}
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                    age = value;
+                else
+                    age = 0;
+            }
+        }
+
         #endregion
 
         #region --------- Methods -----------
 
         // Metoder
+
+        public override string ToString()
+        {
+            string s = "\nDjurets Detaljer: \nNamn: " + Name + "\nÅlder: " + Age;
+            return s;
+        }
 
         #endregion
     }
